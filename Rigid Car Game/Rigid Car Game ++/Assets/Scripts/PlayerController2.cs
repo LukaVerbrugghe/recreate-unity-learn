b052ea9e355f3aee8ed0334d716d4d3fd71c546a
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-01-04 16:00:04 +0100

    WORK

--- Rigid Car Game/Rigid Car Game ++/Assets/Scripts/PlayerController2.cs
+++ Rigid Car Game/Rigid Car Game ++/Assets/Scripts/PlayerController2.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerController2 : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    private void Update() {
         
     }
 }
