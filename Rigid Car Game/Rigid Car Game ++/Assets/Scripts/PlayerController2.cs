c16e69e117ce561210570c1b7aed7881fed820e1
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-01-05 10:48:55 +0100

    Revert "WORK"
    This reverts commit b052ea9e355f3aee8ed0334d716d4d3fd71c546a.

--- Rigid Car Game/Rigid Car Game ++/Assets/Scripts/PlayerController2.cs
+++ Rigid Car Game/Rigid Car Game ++/Assets/Scripts/PlayerController2.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController2 : MonoBehaviour {
+public class PlayerController2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-
-    private void Update() {
+    // Update is called once per frame
+    void Update()
+    {
         
     }
 }
